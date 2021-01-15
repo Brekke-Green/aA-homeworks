1bfe35cb14bb1a1e4c8f6976d2f11a8e1b0d58a7
Brekke Green <brekke.andrew.green@gmail.com>
2021-01-15 00:31:32 -0500

    Created Stack#push, Stack#pop, and Stack#peek

--- W3D5/stack.rb
+++ W3D5/stack.rb
@@ -3,4 +3,15 @@ class Stack
         @stack = []
     end 
 
+    def push(el)
+        @stack.push(el)
+    end 
+
+    def pop 
+        @stack.pop
+    end 
+
+    def peek
+        @stack[-1]
+    end 
 end 
