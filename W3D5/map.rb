8932bb5612f0dfdc70edf91309b126b99bc3288f
Brekke Green <brekke.andrew.green@gmail.com>
2021-01-15 01:18:49 -0500

    Created Map#set, Map#get, Map#delete, and Map#show

--- W3D5/map.rb
+++ W3D5/map.rb
@@ -3,4 +3,23 @@ class Map
         @map = []
     end 
 
+    def set(key, value)
+        @map.push([key, value]) if @map.length == 0
+
+        keys = @map.map {|el| el[0]}
+        keys.include?(key) ? @map[keys.index(key)][1] = value : @map.push([key, value])
+    end 
+
+    def get(key)
+        key_value = @map.select {|el| el[0] == key}
+        key_value[0][1]
+    end 
+
+    def delete(key)
+        @map.select! {|el| el[0] != key}
+    end 
+
+    def show
+        p @map
+    end 
 end 
