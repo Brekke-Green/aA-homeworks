698db18cd8a3525b41953f8e2e5fa0f9a582bbe7
Brekke Green <brekke.andrew.green@gmail.com>
2021-01-17 16:12:58 -0500

    Completed GraphNode and bfs method

--- W4D1/graph_node.rb
+++ W4D1/graph_node.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class GraphNode
 
     attr_accessor :value, :neighbors
@@ -6,3 +8,33 @@ class GraphNode
         @neighbors = []
     end 
 end 
+
+def bfs(starting_node, target_value)
+
+    queue = [starting_node]
+    visited = Set.new()
+
+    until queue.empty?
+        check = queue.shift
+        return check if check.value == target_value 
+        check.neighbors.each do |neighbor|
+            queue << neighbor if !visited.include?(neighbor.value)
+            visited << neighbor.value
+        end 
+    end 
+    nil
+end 
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+
+p bfs(a, "b")
+p bfs(a, "f")
