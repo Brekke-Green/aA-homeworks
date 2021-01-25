eaa81524d008e755adee8ab238b49fdcea008be9
Brekke Green <brekke.andrew.green@gmail.com>
2021-01-24 22:56:28 -0500

    Fixed LRUCache#add and fixed underlying data structures used

--- W5D1/lru_cache.rb
+++ W5D1/lru_cache.rb
@@ -1,25 +1,53 @@
 class LRUCache
     def initialize(size)
         @size = size 
-        @underlying_cache = Array.new(4)
+        @underlying_queue = Queue.new
+        @underlying_hash = Hash.new
     end
 
     def count
       # returns number of elements currently in cache
-        @underlying_cache.count {|el| el != nil}
+        @underlying_queue.length
     end
 
     def add(el)
       # adds element to cache according to LRU principle
-        
+        if count == @size 
+            @underlying_queue.shift
+            @underlying_queue.push(el)
+            @underlying_hash[el] 
+        else 
+            @underlying_queue.push(el)
+            @underlying_hash[el] 
+        end 
     end
 
     def show
       # shows the items in the cache, with the LRU item first
-        puts @underlying_cache
+        
     end
 
     private
     # helper methods go here!
 
   end
+
+  johnny_cache = LRUCache.new(4)
+
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(5)
+
+  puts johnny_cache.count # => returns 2
+
+  johnny_cache.add([1,2,3])
+  johnny_cache.add(5)
+  johnny_cache.add(-5)
+  johnny_cache.add({a: 1, b: 2, c: 3})
+  johnny_cache.add([1,2,3,4])
+  johnny_cache.add("I walk the line")
+  johnny_cache.add(:ring_of_fire)
+  johnny_cache.add("I walk the line")
+  johnny_cache.add({a: 1, b: 2, c: 3})
+
+
+  johnny_cache.show
