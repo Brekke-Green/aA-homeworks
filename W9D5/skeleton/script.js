313525639ae0b2b4575cac2027a38cbcc3ab911e
Brekke Green <brekke.andrew.green@gmail.com>
2021-03-08 21:48:50 -0500

    Wrap up of W9D5

--- W9D5/skeleton/script.js
+++ W9D5/skeleton/script.js
@@ -16,16 +16,41 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-  // adding SF places as list items
+  const handleFavoriteSubmit = (e) => {e.preventDefault();
+    const favoriteInput = document.querySelector(".favorite-input");
+    const favorite = favoriteInput.value;
+    favoriteInput.value = "";
+    const newListLi = document.createElement("li");
+    newListLi.textContent = favorite;
+    const favoritesList = document.getElementById("sf-places");
+    favoritesList.appendChild(newListLi);
+  };
 
-  // --- your code here!
+  const listSubmitButton = document.querySelector(".favorite-submit");
+  listSubmitButton.addEventListener("click",handleFavoriteSubmit);  
+  const showPhotoForm = (e) => {const photoFormDiv=document.querySelector(".photo-form-container");
 
+    if (photoFormDiv.className === "photo-form-container") {
+      photoFormDiv.className = "photo-form-container hidden";
+    } else {
+      photoFormDiv.className = "photo-form-container";
+    }
+  };
 
-
-  // adding new photos
-
-  // --- your code here!
-
-
-
+  const photoFormShowButton = document.querySelector(".photo-show-button");
+  photoFormShowButton.addEventListener("click",showPhotoForm);
+  const handlePhotoSubmit = (e) => {e.preventDefault();
+    const photoUrlInput = document.querySelector(".photo-url-input");
+    const photoUrl = photoUrlInput.value;
+    photoUrlInput.value = "";
+    const newImg = document.createElement("img");
+    newImg.src = photoUrl;
+    const newPhotoLi = document.createElement("li");
+    newPhotoLi.appendChild(newImg);
+    const dogPhotosList = document.querySelector(".dog-photos");
+    dogPhotosList.appendChild(newPhotoLi);
+  };
+  
+  const photoSubmitButton = document.querySelector(".photo-url-submit");
+  photoSubmitButton.addEventListener("click",handlePhotoSubmit);
 });
